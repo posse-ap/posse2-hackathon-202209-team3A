3e7038b9b5ef7ce9b603e5081921e7e4b7776797
Takumi Hara <takumihara1226@gmail.com>
2022-09-05 09:09:09 +0900

    feat: script to create quiz listing file

---
+++ script/create_quiz_listing.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+const quizDir = "./quiz";
+
+const walk = (p, callback) => {
+  const results = [];
+
+  fs.readdir(p, (err, files) => {
+    if (err) {
+      throw err;
+    }
+
+    let pending = files.length;
+    if (!pending) {
+      return callback(null, results);
+    }
+
+    files
+      .map((file) => {
+        return path.join(p, file);
+      })
+      .filter((file) => {
+        if (fs.statSync(file).isDirectory())
+          walk(file, function (err, res) {
+            results.push({ name: path.basename(file), children: res });
+            if (!--pending) {
+              callback(null, results);
+            }
+          });
+        return fs.statSync(file).isFile();
+      })
+      .forEach((file) => {
+        const fileName = path.basename(file);
+        results.push({ file, name: getNameWithoutExt(fileName) });
+        if (!--pending) {
+          callback(null, results);
+        }
+      });
+  });
+};
+
+// does not expect the name to have multiple dots
+const getNameWithoutExt = (file) => {
+  return file.split(".")[0];
+};
+
+walk(quizDir, function (err, results) {
+  if (err) {
+    throw err;
+  }
+  var data = { name: "root", children: results };
+  console.log(JSON.stringify(data));
+});
