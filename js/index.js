415bce8255febd266276069ab015d127cd2f702e
Takumi Hara <takumihara1226@gmail.com>
2022-09-11 21:03:57 +0900

    feat: quiz listing in toppage

--- js/index.js
+++ js/index.js
@@ -9,19 +9,30 @@ const quizImgPath = "./quiz_img";
   const shufflePeople = shuffleArray(people);
 
   setupProfiles(shufflePeople, 0);
+
+  const button = document.getElementById("js-button");
 });
 
 const setupProfiles = (people) => {
+  console.log(people);
   const profiles = document.getElementById("js-profiles");
   const profileTemplate = document.getElementById("js-templ-profile");
 
-  const names = people.map((person) => person.name);
-  names.forEach((name) => {
+  people.forEach((person) => {
+    const name = person.name;
     const profile = profileTemplate.cloneNode(true);
     profile.removeAttribute("id");
     // tood(takumi) add default image
     profile.querySelector("img").src = `${quizImgPath}/${name}.jpg`;
     profile.querySelector("dt").textContent = `3.0期生 ${name}`;
     profiles.appendChild(profile);
+
+    const choiceWrapper = profile.querySelector(".js-choiceWrapper");
+    person.children.forEach((quiz) => {
+      const a = document.createElement("a");
+      a.href = `./quiz/index.html?person_name=${name}&quiz_name=${quiz.name}`;
+      a.textContent = quiz.name;
+      choiceWrapper.appendChild(a);
+    });
   });
 };
